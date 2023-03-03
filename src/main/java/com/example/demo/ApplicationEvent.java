74b2e3d18f48f59d0a2bf820bf7fad231983da51
NhatVu <minhnhatse.uit@gmail.com>
2023-03-03 22:50:27 +0000

    first commit

---
+++ src/main/java/com/example/demo/ApplicationEvent.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationEvent {
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println("hello world, Running after application started");
+    }
+
+}
---
+++ src/main/java/com/example/demo/prettier/Hello.java
@@ -0,0 +1,7 @@
+package com.example.demo.prettier;
+
+public class Hello {
+    public int sum(int a, int b){
+        return a + b;
+    }
+}
